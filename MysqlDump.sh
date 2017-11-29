937831b829a8390988951ef2283d7758abbe7ebb
carlos coelho <carloscoelho_@msn.com>
2017-11-29 13:59:56 -0200

    Update MysqlDump.sh

--- MysqlDump.sh
+++ MysqlDump.sh
@@ -1,24 +1,23 @@
-#Backup MySQL database daily using cron job
-#1) create a bk directory under home directory, in my case /home/jiansen,
-#cd bk
-#) Create a shell scrip MySQLdump.sh
-#mysqldump  -u root -pmypass mydb | gzip >MySQLDB_`date +"%Y%m%d"`.sql.gz
-#replace root,  mypass, mydb to your databse username, password and database name
-#date +"%Y%m%d
-#is shell comand to produce year, month and day, for example  20140603
-#example output
-# MySQLDB_20140603.sql.gz
-#3) Create a cron job
- #crontab -e
-#30 23 * * * /home/jiansen/bk/MySQLdump.sh 2>&1>>/home/jiansen/bk/mysqbackup.log
-#each night  23:30pm run  /home/jiansen/bk/MySQLdump.sh and dump errors
-#to logfile 
-#4) to list cron job
-#  crontab -l
-#5) To remove cron job
-#  crontab -r
+#!/bin/bash
 
+#######################################
+# Backup diario das bases do MySQL    
+# Criado por Felipe Savoia                  
+#######################################
 
+#VARIAVEIS
+DATAHORA=`date +%Y%m%d-%H%M`
+MYSQLDUMP=/usr/bin/mysqldump
+MYSQLDIR='cd /var/www/html/msqlbackup/'
+TAR=/bin/tar
+RM=/bin/rm
 
-#!/bin/sh
-mysqldump  -u root -p1q2w3e ccstudio | gzip >MySQLDB_`date +"%Y%m%d"`.sql.gz
+#Realizando o backup de todas as bases
+$MYSQLDUMP -uroot -p1q2w3e  --all-databases > /var/www/html/msqlbackup/mysql.bkp_$DATAHORA
+
+#Entrando no diretorio de backup
+$MYSQLDIR
+
+#Compactando o backup
+$TAR czvf mysql.bkp_$DATAHORA.tar.gz mysql.bkp_$DATAHORA
+$RM mysql.bkp_$DATAHORA
