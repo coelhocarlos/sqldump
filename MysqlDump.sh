2da2c829fb78ce17f3a24076a90a0aea89efa371
carlos coelho <carloscoelho_@msn.com>
2018-04-13 22:12:33 -0300

    Update MysqlDump.sh

--- MysqlDump.sh
+++ MysqlDump.sh
@@ -1,23 +1,80 @@
 #!/bin/bash
 
-#######################################
-# Backup diario das bases do MySQL    
-# Criado por Felipe Savoia                  
-#######################################
+# Add your backup dir location, password, mysql location and mysqldump location
+#DATE=$(date +%d-%m-%Y)
+#BACKUP_DIR="/var/www/html/msqlbackup"
+#MYSQL_USER="root"
+#MYSQL_PASSWORD="1q2w3e"
+#MYSQL=/usr/bin/mysql
+#MYSQLDUMP=/usr/bin/mysqldump
 
-#VARIAVEIS
-DATAHORA=`date +%Y%m%d-%H%M`
-MYSQLDUMP=/usr/bin/mysqldump
-MYSQLDIR='cd /var/www/html/msqlbackup/'
-TAR=/bin/tar
-RM=/bin/rm
+# To create a new directory into backup directory location
+#mkdir -p $BACKUP_DIR/$DATE
+#chown -R zombie:zombie $BACKUP_DIR/$DATE
+#chmod 777 $BACKUP_DIR/$DATE
 
-#Realizando o backup de todas as bases
-$MYSQLDUMP -uroot -p1q2w3e  --all-databases > /var/www/html/msqlbackup/mysql.bkp_$DATAHORA
 
-#Entrando no diretorio de backup
-$MYSQLDIR
+#get a list of databases
+#databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e  "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|phpmyadmin)"`
 
-#Compactando o backup
-$TAR czvf mysql.bkp_$DATAHORA.tar.gz mysql.bkp_$DATAHORA
-$RM mysql.bkp_$DATAHORA
+
+# dump each database in separate name
+#for db in $databases; do
+#echo $db
+#$MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip >$BACKUP_DIR/mysql/$db.gz
+#done
+
+# Delete files older than 10 days
+#find $BACKUP_DIR/* -mtime +10 -exec rm {} \;
+
+
+#!/bin/bash
+# Simple script to backup MySQL databases
+
+# Parent backup directory
+backup_parent_dir="/var/www/html/msqlbackup"
+
+# MySQL settings
+mysql_user="root"
+mysql_password="1q2w3e"
+
+# Read MySQL password from stdin if empty
+if [ -z "${mysql_password}" ]; then
+  echo -n "Enter MySQL ${mysql_user} password: "
+  read -s mysql_password
+  echo
+fi
+
+# Check MySQL password
+echo exit | mysql --user=${mysql_user} --password=${mysql_password} -B 2>/dev/null
+if [ "$?" -gt 0 ]; then
+  echo "MySQL ${mysql_user} password incorrect"
+  exit 1
+else
+  echo "MySQL ${mysql_user} password correct."
+fi
+
+# Create backup directory and set permissions
+backup_date=`date +%Y-%m-%d`
+backup_dir="${backup_parent_dir}/${backup_date}"
+echo "Backup directory: ${backup_dir}"
+mkdir -p "${backup_dir}"
+chmod 777 "${backup_dir}"
+
+# Get MySQL databases
+mysql_databases=`echo 'show databases' | mysql --user=${mysql_user} --password=${mysql_password} -B | sed /^Database$/d`
+
+# Backup and compress each database
+for database in $mysql_databases
+do
+  if [ "${database}" == "information_schema" ] || [ "${database}" == "performance_schema" ]; then
+        additional_mysqldump_params="--skip-lock-tables"
+  else
+        additional_mysqldump_params=""
+  fi
+  echo "Creating backup of \"${database}\" database"
+  mysqldump ${additional_mysqldump_params} --user=${mysql_user} --password=${mysql_password} ${database} | gzip > "${backup_dir}/${database}.gz"
+  chmod 777 "${backup_dir}/${database}.gz"  
+  #Delete files older than 10 days
+  find "${backup_dir}/"* -mtime +10 -exec rm {} \;
+done
